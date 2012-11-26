c1f7f0c5d4ada629faa6529b6b269c7fc4270d9c
Patrick Evans <ptevans@gmail.com>
2012-11-26 12:27:15 -0500

    moving more code

--- main.go
+++ main.go
@@ -1,29 +1,8 @@
 package main
 
-import (
-	"gojabberd/server"
-	"net"
-	"os"
-)
+import "gojabberd/server"
 
 func main() {
-	println("Starting XMPP Server")
-
-	//domainTable := new(server.DomainTable)
-	presenceTable := new(server.PresenceTable)
-	presenceTable.Users = make(map[string](*server.ClientConnection))
-	listener, err := net.Listen("tcp", "0.0.0.0:5222")
-	if err != nil {
-		println("error!", err.Error())
-		os.Exit(1)
-	}
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			println("error!", err.Error())
-			return
-		}
-		c := new(server.ClientConnection)
-		go c.Go(conn, presenceTable)
-	}
+	server.Start()
 }
+
--- server/server.go
+++ server/server.go
@@ -1,51 +1,29 @@
 package server
 
 import (
-	"errors"
+	"net"
+	"os"
 )
 
-type DomainTable struct {
-	domains	map[string](*Domain)
-}
+func Start() {
+	println("Starting XMPP Server")
 
-// Register a domain with the global domain table. If the domain is already
-// registered, an error will be returned.
-func (dt *DomainTable) RegisterDomain(d *Domain) (e error) {
-	exists := dt.CheckDomainRegistration(d)
-	if exists {
-		e = errors.New("Domain already registered!")
-		return
+	//domainTable := new(DomainTable)
+	presenceTable := new(PresenceTable)
+	presenceTable.Users = make(map[string](*ClientConnection))
+	listener, err := net.Listen("tcp", "0.0.0.0:5222")
+	if err != nil {
+		println("error!", err.Error())
+		os.Exit(1)
 	}
-	dt.domains[d.domain] = d
-	for _, alias := range d.aliases {
-		dt.domains[alias] = d
-	}
-	return
-}
-
-// Test to see if a Domain is registered in the domain table, this checks the
-// primary domain name as well as any aliases.
-func (dt *DomainTable) CheckDomainRegistration(d *Domain) (bool) {
-	_, exists := dt.domains[d.domain]
-	if exists {
-		return true
-	}
-	for _, alias := range d.aliases {
-		_, exists = dt.domains[alias]
-		if exists {
-			return true
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			println("error!", err.Error())
+			return
 		}
+		c := new(ClientConnection)
+		go c.Go(conn, presenceTable)
 	}
-	return false
-}
-
-// Lookup a domain and return a pointer to the Domain. Returns an error if the
-// domain cannot be found.
-func (dt *DomainTable) GetDomain(hostname string) (*Domain, error) {
-	d, ok := dt.domains[hostname]
-	if ok {
-		return d, nil
-	}
-	return nil, errors.New("Domain could not be found!")
 }
 
